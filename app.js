15615b59e682229767cf0f4c47ab96a2f521290a
Geovana Moreira de Oliveira <geovanamoreira907@gmail.com>
2025-08-13 23:30:29 -0300

    Primeiros passos

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Amigo Secreto');
+    exibirTextoNaTela('h2', 'Digite o nome dos seus amigos');
+}
+
+exibirMensagemInicial();
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if(nome === ''){
+        exibirTextoNaTela('h2', 'Por favor, digite um nome');
+        return;
+    }
+
+    //confere se o nome foi repetido
+    else{
+        if(listaAmigos.includes(nome)){
+            exibirTextoNaTela('h2', 'Esse nome ja foi inserido');
+            return;
+        }
+    }
+
+    listaAmigos.push(nome);
+    console.log(listaAmigos);
+    limparCampo();
+}
+
+function sortearAmigo() {
+
+    if (listaAmigos.length < 4) {
+        exibirTextoNaTela('h2', 'Insira ao menos 4 nomes');
+    }
+   
+    else{
+    }
+}
+
+function limparCampo(){
+    nome = document.querySelector('input');
+    nome.value = '';
+}
