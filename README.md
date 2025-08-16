166b08d9df2c43865e27475d7f7966ef9c66427c
Geovana Moreira de Oliveira <geovanamoreira907@gmail.com>
2025-08-16 20:32:42 -0300

    Finalizando

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Desafio Amigo Secreto
+![alt text](assets/Imagem-Site.png)
+
+O projeto consiste em um site prático e funcional para a dinâmica de amigo secreto. Nele, o usuário deve informar no mínimo 4 nomes clicando no botão "Adicionar", que irá registrá-los e mostrá-los em formato de lista.
+
+Ao informar os nomes desejados, o botão "Sortear Amigo" irá sortear aleatoriamente um dos participantes, assim liberando o recurso de reiniciar o sorteio, no qual os dados são limpos e pode-se iniciar a dinâmica novamente.
+
+![alt text](image.png)
+
+Linguagens Utilizadas:
+- HTML;
+- CSS;
+- JavaScript;
+
+Autora:
+https://github.com/geovanamoreira
--- app.js
+++ app.js
@@ -15,35 +15,68 @@ exibirMensagemInicial();
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
 
-    if(nome === ''){
+
+    if (nome === '') {
         exibirTextoNaTela('h2', 'Por favor, digite um nome');
         return;
     }
 
     //confere se o nome foi repetido
-    else{
-        if(listaAmigos.includes(nome)){
+    else {
+        if (listaAmigos.includes(nome)) {
             exibirTextoNaTela('h2', 'Esse nome ja foi inserido');
+            limparCampo();
             return;
         }
     }
 
     listaAmigos.push(nome);
+    arrumarLista();
+
     console.log(listaAmigos);
     limparCampo();
 }
 
+function arrumarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    // adiciona os nomes no formato certo de lista
+    listaAmigos.forEach((nome) => {
+        let item = document.createElement('li');
+        item.textContent = nome;
+        lista.appendChild(item);
+    });
+}
+
 function sortearAmigo() {
 
+    //se tiver menos de 4 pessoas ele pede por mais nomes
     if (listaAmigos.length < 4) {
         exibirTextoNaTela('h2', 'Insira ao menos 4 nomes');
     }
-   
-    else{
+
+    else {
+        let sorteioNome = Math.floor(Math.random() * listaAmigos.length);
+        
+        //pega pelo nome, nao pelo número do indice
+        let nomeSorteado = listaAmigos[sorteioNome];
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `O nome sorteado foi ${nomeSorteado}`;
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }
 }
 
-function limparCampo(){
+function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function reiniciarTudo() {
+    document.getElementById('resultado').innerHTML = '';
+    listaAmigos = [];
+    arrumarLista();
+    exibirMensagemInicial();
 }
